556a1f2495779689fb885033f6f03d452d0170bc
Amo-QAQ <969736717@qq.com>
2026-08-24 18:53:46 +0800

    资产导出

--- Aseprite/Batch export/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-chcp 65001 >nul 2>&1
-cd /d "%~dp0"
-where python >nul 2>&1 && python "sprite_sheet_combiner.py" || (echo [ERROR] Python not found! Please install Python from python.org & pause)
-pause
