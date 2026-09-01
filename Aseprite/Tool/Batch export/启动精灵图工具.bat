ee4af8e8fa6d1e18ee6ef96bd3c747fe32e1a710
Amo-QAQ <969736717@qq.com>
2026-09-01 19:06:15 +0800

    工具更新

--- Aseprite/Tool/Batch export/启动精灵图工具.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title Sprite Sheet Generator
-cd /d "%~dp0"
-set PYTHONPATH=C:\Users\admin\.workbuddy\binaries\python\envs\default\Lib\site-packages
-"C:\Users\admin\.workbuddy\binaries\python\versions\3.13.12\python.exe" "sprite_sheet_generator.py"
-pause
